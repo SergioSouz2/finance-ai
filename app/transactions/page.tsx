77061a54964eb2b5a6ca22310d12908d3fe2b671
Sergio Souz2 <sergio.tbl0123@gmail.com>
2025-05-03 01:16:01 -0300

    feat: add navbar components

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -7,17 +7,19 @@ const TransactionsPage = async () => {
   const transactions = await db.transactions.findMany({});
 
   return (
-    <div className="space-y-6 p-6">
-      <div className="mb-4 flex w-full items-center justify-between">
-        <h1 className="text-2xl font-bold">Transações</h1>
-        <AddTransactionButton />
-      </div>
+    <>
+      <div className="space-y-6 p-6">
+        <div className="mb-4 flex w-full items-center justify-between">
+          <h1 className="text-2xl font-bold">Transações</h1>
+          <AddTransactionButton />
+        </div>
 
-      <DataTable
-        columns={TransctionColumns}
-        data={JSON.parse(JSON.stringify(transactions))}
-      />
-    </div>
+        <DataTable
+          columns={TransctionColumns}
+          data={JSON.parse(JSON.stringify(transactions))}
+        />
+      </div>
+    </>
   );
 };
 export default TransactionsPage;
